1ab11d0203ceaa157cc98ba853756da95bd1237d
JulianArana777 <julianaaranag@gmail.com>
2026-02-11 18:29:28 -0500

    Added header and navbar

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,23 +1,16 @@
-import ButtonGradient from "../assets/svg/ButtonGradient"
-import Button from "../Components/Button"
-const HomePage = () =>{
+import ButtonGradient from "../assets/svg/ButtonGradient";
+import Header from "../Components/Header";
 
-
-
- return(
+const HomePage = () => {
+  return (
     <>
-    <h1 className="text-3xl font-bold underline">
-        Hello World
-        <div className="pt-19 lg:pt-21 overflow-hidden" >
-            
-            <Button className="mt-10" href="#login">
-                something
-            </Button >
-            </div>       
-    </h1>
-     <ButtonGradient></ButtonGradient>
-  </>
- )
+      <div className="pt-19 lg:pt-21 overflow-hidden">
+        <Header />
+      </div>
+
+      <ButtonGradient />
+    </>
+  );
 };
 
-export default HomePage
+export default HomePage;
