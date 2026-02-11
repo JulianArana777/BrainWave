7334b8957f5b994c95dedf1b2fcf4814de6ffd29
JulianArana777 <julianaaranag@gmail.com>
2026-02-11 17:46:57 -0500

    Improved botton component

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -5,15 +5,25 @@ const Button = ({className, href, onClick, children, px, white}) =>{
     
     const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 
     ${px || 'px-7'} ${white ? 'text-n-1' : 'text-n-8'} ${className || ""}`
+    const spanClasses = 'relative z-10'
     const renderButton = () => (
         <button  className={classes}>
-            <span>{children}</span>
+            <span className={spanClasses}>{children}</span>
             {ButtonSvg(white)}
         </button>
         
     )
-    return renderButton();
+    
 
+    const renderlink = () =>(
+        <a href= {href} className={classes}>
+            <span className={spanClasses}>{children}</span>
+            {ButtonSvg(white)}
+        </a>       
+    )
+
+     return href ? renderlink() : renderButton()
+    
    
 }
  export default Button
